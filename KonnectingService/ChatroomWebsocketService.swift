e26b940845ab926babe87ad535d559cd107341b7
Christian.Starly <yoh.chrisitan@gmail.com>
2023-04-22 13:51:21 -0400

    creating a protocol for chatroomwebsocketapi to implement in chatroomwebsocketservice

--- KonnectingService/ChatroomWebsocketService.swift
+++ KonnectingService/ChatroomWebsocketService.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
+public protocol ChatroomWebSocketAPI {
+
+    func login(username: String, email: String)
+}
+
 public class ChatroomWebsocketService {
 
+}
+
+extension ChatroomWebsocketService: ChatroomWebSocketAPI {
+
+    public func login(username: String, email: String) {
+
+    }
 }
