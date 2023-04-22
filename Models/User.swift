3f5218360a82167045e9fa1e305274d8cecf25ee
Christian.Starly <yoh.chrisitan@gmail.com>
2023-04-22 13:46:36 -0400

    implement userModel

--- Models/User.swift
+++ Models/User.swift
@@ -9,5 +9,12 @@ import Foundation
 
 public struct User {
 
-    
+    public let email: String
+    public let username: String
+
+    init(email: String, username: String) {
+        
+        self.email = email
+        self.username = username
+    }
 }
